2622c3cb818c5db415175bfa255d823d7fd2b5ce
jaysmith13 <janeasmith13@gmail.com>
2022-11-12 18:45:57 -0700

    Update message.test.js

---
+++ Backend/server/utils/message.test.js
@@ -0,0 +1,13 @@
+const { describe, it } = require('node:test');
+var {generateMessage, generateLocationMessage} = require('./message');
+
+describe('generateMessage', () => {
+    it('Should generate correct message object', () => {
+        var from = 'Jen';
+        var room = 'Node JS';
+        var text = 'Some msg';
+        var message = generateMessage(from, room, text);
+
+        expect(typeof message.createData).toBe('number');
+    })
+})
